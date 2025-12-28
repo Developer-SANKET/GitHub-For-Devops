2a97056f5330aa22d86c1a5794464302b3f8e51c
Developer-SANKET <psanket6.2000@gmail.com>
2025-12-28 12:41:16 +0530

    here is the code

---
+++ testing.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+x = 10  # unused variable (pylint warning)
+
+
+def add(a, b):
+    return a + b
+
+
+def multiply(a,b):   # spacing issue
+    return a*b       # missing spaces
+
+
+def print_value():
+    print("Value is:", y)   # undefined variable (error)
+
+
+def long_function_name_with_many_parameters(param1, param2, param3, param4):
+    return param1 + param2 + param3 + param4
+
+
+print(add(5, 3))
